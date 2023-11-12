e57875fb695547543606de48159f83c0378ee936
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2023-11-12 16:42:59 +0330

    add verify screen

--- lib/features/auth/verify_screen.dart
+++ lib/features/auth/verify_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:pinput/pinput.dart';
+
+import '../../core/utils/routes.dart';
+import '../../core/widgets/custom_button.dart';
 
 class VerifyScreen extends StatefulWidget {
   const VerifyScreen({super.key});
@@ -9,6 +13,27 @@ class VerifyScreen extends StatefulWidget {
 }
 
 class _VerifyScreenState extends State<VerifyScreen> {
+  final textPinNode = FocusNode();
+
+  final defaultPinTheme = PinTheme(
+    width: 56,
+    height: 60,
+    margin: const EdgeInsets.symmetric(horizontal: 5),
+    textStyle: const TextStyle(
+      fontSize: 20,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    decoration: BoxDecoration(
+      color: const Color(0xffECECEC),
+      border: Border.all(
+        color: Colors.black38,
+        width: 1,
+      ),
+      borderRadius: BorderRadius.circular(9),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,10 +87,93 @@ class _VerifyScreenState extends State<VerifyScreen> {
               ],
             ),
             const Gap(24),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(32, 64, 32, 0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 64, 32, 0),
               child: Column(
-                children: [],
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'کد چهار رقمی ارسال شده را وارد کنید',
+                        style: TextStyle(
+                          color: Color(0xff20437B),
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Gap(32),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: Pinput(
+                        autofocus: true,
+                        keyboardType: TextInputType.number,
+                        pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                        androidSmsAutofillMethod:
+                            AndroidSmsAutofillMethod.smsRetrieverApi,
+                        focusedPinTheme: defaultPinTheme.copyWith(
+                          decoration: BoxDecoration(
+                            color: const Color(0xff406CB1).withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(9),
+                            border: Border.all(
+                              color: const Color(0xff406CB1),
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        defaultPinTheme: defaultPinTheme,
+                        focusNode: textPinNode,
+                      ),
+                    ),
+                  ),
+                  const Gap(64),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: CustomButton(
+                      height: 54,
+                      borderRadius: 12,
+                      showShadow: false,
+                      backgroundColor: const Color(0xff406CB1),
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.home,
+                        );
+                      },
+                      child: const Text(
+                        'تایید',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Gap(24),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'ارسال مجدد کد',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Gap(16),
+                      Text(
+                        '01:48',
+                        style: TextStyle(
+                          color: Color(0xff406CB1),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
             const Gap(32),
