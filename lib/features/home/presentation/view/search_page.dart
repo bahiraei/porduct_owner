20e16470f893664501236e6106239f6a5c9b296c
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2023-11-14 09:52:15 +0330

    change search screen ui

--- lib/features/home/presentation/view/search_page.dart
+++ lib/features/home/presentation/view/search_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
 class SearchPageView extends StatelessWidget {
   const SearchPageView({super.key});
@@ -7,54 +6,77 @@ class SearchPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(
-          24,
-          0,
-          24,
-          0,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Gap(MediaQuery.of(context).viewPadding.top + 24),
-              TextField(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              alignment: Alignment.center,
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 32,
+              ),
+              decoration: const BoxDecoration(
+                color: Color(0xff001E4D),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(48),
+                ),
+              ),
+              child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(24),
                   ),
-                  suffixIcon: const Icon(
+                  suffixIcon: Icon(
                     Icons.search,
+                    color: Colors.white.withOpacity(0.5),
                   ),
                   contentPadding: const EdgeInsets.symmetric(
                     horizontal: 16,
                     vertical: 16,
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
                   hintText: 'جستجو کنید...',
+                  hintStyle: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontWeight: FontWeight.w400,
+                  ),
                   filled: true,
                   fillColor: Colors.grey.withOpacity(0.3),
                 ),
-                /*onTapOutside: (event) => FocusScope.of(context).unfocus(),*/
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
                 autofocus: false,
                 style: const TextStyle(
                   fontSize: 14,
                 ),
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.75,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/search/no_result.png',
-                      width: 500,
-                      height: MediaQuery.of(context).size.height * 0.8 * 0.7,
-                    ),
-                  ],
-                ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/search/no_result.png',
+                    width: 500,
+                    height: MediaQuery.of(context).size.height * 0.6 * 0.7,
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
